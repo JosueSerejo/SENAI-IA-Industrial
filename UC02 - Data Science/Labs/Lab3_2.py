ad165c096e58e14b73a83ab3f4b01f7afe3905de
Josué Serejo <josueserejo5@gmail.com>
2025-12-10 14:30:31 -0300

    Adicionando mapa de calor

--- UC02 - Data Science/Labs/Lab3_2.py
+++ UC02 - Data Science/Labs/Lab3_2.py
@@ -2,6 +2,7 @@ import warnings, requests, zipfile, io
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import arff
+import seaborn as sns
 
 f_zip = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00212/vertebral_column_data.zip'
 
@@ -9,7 +10,6 @@ r = requests.get(f_zip, stream=True)
 
 vertebral_zip = zipfile.ZipFile(io.BytesIO(r.content)) 
 vertebral_zip.extractall(path='data/')
-
 
 data = arff.loadarff('data/column_2C_weka.arff')
 
@@ -46,6 +46,21 @@ df['class'].value_counts()
 
 class_mapper = {b'Abnormal': 1, b'Normal': 0}
 
-df['class'] = df['class'].replace(class_mapper).astype(int)
+df['class'] = df['class'].map(class_mapper).astype(int)
 
 df.plot.scatter(y='degree_spondylolisthesis', x='class')
+
+df.groupby('class').boxplot(fontsize=20, rot=90, figsize=(10, 10), patch_artist=True)
+plt.show() 
+
+corr_matrix = df.corr(numeric_only=True)
+corr_matrix["class"].sort_values(ascending=False)
+
+pd.plotting.scatter_matrix(df, figsize=(12, 12))
+
+plt.show()
+
+plt.figure(figsize=(10, 10)) 
+sns.heatmap(corr_matrix, cmap='coolwarm', annot=True, fmt='.2f')
+
+plt.show()
