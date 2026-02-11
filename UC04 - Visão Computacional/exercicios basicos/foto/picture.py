b09a68d26e304f171c5eeb29bdb4ec08b50faa5b
Josué Serejo <josueserejo5@gmail.com>
2026-02-11 20:53:23 -0300

    Aplicando mascara preto e branco

--- UC04 - Visão Computacional/exercicios basicos/foto/picture.py
+++ UC04 - Visão Computacional/exercicios basicos/foto/picture.py
@@ -2,12 +2,15 @@ import cv2
 
 imagem = cv2.imread('imagem.jpg')
 
+imagem = cv2.resize(imagem, (800, 800))
+
 imagem_pb = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-redimensionada = cv2.resize(imagem_pb, (800, 800))
+_, mask = cv2.threshold(imagem_pb, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-cv2.imshow('Imagem', redimensionada)
+resultado = cv2.bitwise_and(imagem_pb, imagem_pb, mask=mask)
+
+cv2.imshow('Resultado Preto e Branco', resultado)
 
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
